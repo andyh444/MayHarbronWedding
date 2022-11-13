1b94cc07e4417e497542c2e1395ed47f342bc8fd
andyh444 <47712158+andyh444@users.noreply.github.com>
2022-11-13 22:04:43 +0000

    v2 site update

--- mainv2.js
+++ mainv2.js
@@ -2,7 +2,11 @@ function headerItemClick(index) {
 	console.log("hello");
 	var elements = document.getElementsByClassName("headerItem");
 	var pages = document.getElementsByClassName("page");
+	var previousActiveIndex = -1;
 	for (var i = 0; i < elements.length; i++) {
+		if (elements[i].dataset.activepage === "true") {
+			previousActiveIndex = i;
+		}
 		if (i === index) {
 			elements[i].dataset.activepage = true;
 			pages[i].dataset.activepage = true;
@@ -12,10 +16,11 @@ function headerItemClick(index) {
 			pages[i].dataset.activepage = false;
 		}
 	}
+	console.log("prev active index: " + previousActiveIndex);
 	if (index != 1) {
 		clearPolaroids();
 	}
-	else {
+	else if (previousActiveIndex != 1) {
 		initialisePolaroids();
 	}
 }
